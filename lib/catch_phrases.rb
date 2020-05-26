629b68a8fa2e14ba9e136ee3eea9a26b6091ab93
Aian Soktoev <soktoevayan@gmail.com>
2020-05-26 19:33:31 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,23 @@
+phrase = "It's-a me, Mario!"
+status ='Thank You Mario! But Our Princess Is In Another Castle!'
+smth = "It's Dangerous To Go Alone! Take This."
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
   puts status
 end
+
+def link 
+  puts smth 
+end
+
+def all_phrases(mario, toadstool, link)
+  puts mario
+  puts toadstool
+  puts link
+end   
+  
+  
